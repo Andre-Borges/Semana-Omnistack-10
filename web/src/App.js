e6ea0ba01c99f6ad085db58bff120bb246abdf8b
André Borges <hoilut@hotmail.com>
2020-04-05 16:45:58 -0300

    Finish web

--- web/src/App.js
+++ web/src/App.js
@@ -1,4 +1,5 @@
-import React, { useState, usEffect } from 'react';
+import React, { useState, useEffect } from 'react';
+import api from './services/api';
 
 // Componente: Bloco isolado de HTML, CSS e JS, o qual não interfere no restante da aplicação
 // Propriedade: Informações que um componente PAI passa para o componente FILHO
@@ -8,150 +9,40 @@ import './global.css';
 import './App.css';
 import './Sidebar.css';
 import './Main.css';
-import { useEffect } from 'react';
+
+import DevItem from './components/DevItem';
+import DevForm from './components/DevForm';
 
 function App() {
-  const [github_username, setGithubUsername] = useState('');
-  const [techs, setTechs] = useState('');
-
-  const [latitude, setLatitude] = useState('');
-  const [longitude, setLongitude] = useState('');
+  const [devs, setDevs] = useState([]);
 
   useEffect(() => {
-    navigator.geolocation.getCurrentPosition(
-      position => {
-        const { latitude, longitude } = position.coords;
+    async function loadDevs() {
+      const response = await api.get('/devs');
+      setDevs(response.data);
+    }
 
-        setLatitude(latitude);
-        setLongitude(longitude);
-      },
-      err => {
-        console.log(err);
-      },
-      {
-        timeout: 30000,
-      },
-    );
+    loadDevs();
   }, []);
 
-  async function handleAddDev(e) {
-    e.preventDefault();
+  async function handleAddDev(data) {
+    const response = await api.post('/devs', data);
+    // ..devs -> pega todo conteudo dentro do array
+    setDevs([...devs, response.data]);
   }
 
   return (
     <div id="app">
       <aside>
         <strong>Cadastrar</strong>
-        <form>
-          <div className="input-block">
-            <label htmlFor="github_username">Usuário do Github</label>
-            <input
-              name="github_username"
-              id="github_username"
-              required
-              value={github_username}
-              onChange={e => setGithubUsername(e.target.value)}
-            />
-          </div>
-
-          <div className="input-block">
-            <label htmlFor="techs">Tecnologias</label>
-            <input
-              name="techs"
-              id="techs"
-              required
-              value={techs}
-              onChange={e => setTechs(e.target.value)}
-            />
-          </div>
-
-          <div className="input-group">
-            <div className="input-block">
-              <label htmlFor="latitude">Latitude</label>
-              <input
-                type="number"
-                name="latitude"
-                id="latitude"
-                required
-                value={latitude}
-                onChange={e => setLatitude(e.target.value)}
-              />
-            </div>
-
-            <div className="input-block">
-              <label htmlFor="longitude">Longitude</label>
-              <input
-                type="number"
-                name="longitude"
-                id="longitude"
-                required
-                value={longitude}
-                onChange={e => setLongitude(e.target.value)}
-              />
-            </div>
-          </div>
-          <button type="submit">Salvar</button>
-        </form>
+        <DevForm onSubmit={handleAddDev} />
       </aside>
 
       <main>
         <ul>
-          <li className="dev-item">
-            <header>
-              <img
-                src="https://avatars3.githubusercontent.com/u/30246642?s=460&u=435097a80d5aa6b19c43193faa294bbb15516cc3&v=4"
-                alt="André Borges"
-              ></img>
-              <div className="user-info">
-                <strong>Diego Fernandes</strong>
-                <span>ReactJs, Node.js</span>
-              </div>
-            </header>
-            <p>CTO @Rocketseat</p>
-            <a href="https://www.google.com.br">Acessar perfil no Github</a>
-          </li>
-          <li className="dev-item">
-            <header>
-              <img
-                src="https://avatars3.githubusercontent.com/u/30246642?s=460&u=435097a80d5aa6b19c43193faa294bbb15516cc3&v=4"
-                alt="André Borges"
-              ></img>
-              <div className="user-info">
-                <strong>Diego Fernandes</strong>
-                <span>ReactJs, Node.js</span>
-              </div>
-            </header>
-            <p>CTO @Rocketseat</p>
-            <a href="https://www.google.com.br">Acessar</a>
-          </li>
-          <li className="dev-item">
-            <header>
-              <img
-                src="https://avatars3.githubusercontent.com/u/30246642?s=460&u=435097a80d5aa6b19c43193faa294bbb15516cc3&v=4"
-                alt="André Borges"
-              ></img>
-              <div className="user-info">
-                <strong>Diego Fernandes</strong>
-                <span>ReactJs, Node.js</span>
-              </div>
-            </header>
-            <p>CTO @Rocketseat</p>
-            <a href="https://www.google.com.br">Acessar</a>
-          </li>
-          <li className="dev-item">
-            <header>
-              <img
-                src="https://avatars3.githubusercontent.com/u/30246642?s=460&u=435097a80d5aa6b19c43193faa294bbb15516cc3&v=4"
-                alt="André Borges"
-              ></img>
-              <div className="user-info">
-                <strong>Diego Fernandes</strong>
-                <span>ReactJs, Node.js</span>
-              </div>
-            </header>
-            <p>CTO @Rocketseat</p>
-            <a href="https://www.google.com.br">Acessar</a>
-          </li>
+          {devs.map((dev) => (
+            <DevItem key={dev._id} dev={dev} />
+          ))}
         </ul>
       </main>
     </div>
